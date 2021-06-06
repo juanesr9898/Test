9b01559840d22d1eb1e6f1aa7f0314578c0d6bb1
Juan Ruiz <esteban.ruiz@utp.edu.co>
2021-06-06 16:03:14 -0500

    Mirando errores

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from logging import raiseExceptions
 from fastapi import FastAPI
+from fastapi_utils.tasks import repeat_every
 from sqlalchemy import engine
 from vista.authoritation import router_auth
 import modelo.tokens_model as tkm
-from modelo.database import engine
+from modelo.database import engine, get_db
 
 app = FastAPI()
 
+@app.on_event("startup")
+@repeat_every(seconds=5, raise_exceptions=True)  # cada minuto
+def remove_expired_tokens_task(db:router_auth.Session=router_auth.Depends(get_db)) -> None:
+    tokens = db.query(tkm.Token).all()
+    print(tokens)
+    print("\n\n")
+    print("H")
+    
 
 tkm.Base.metadata.create_all(engine)
 
